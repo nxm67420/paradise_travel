3583be5984a2324655b8b1019979d5d6f7fc5cae
nxm67420 <46074688+nxm67420@users.noreply.github.com>
2022-02-26 02:49:16 -0600

    "Updated JSON" "Added Component to App" "Updated TripList Component" "TripList CSS Created"

---
+++ src/components/TripList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+//styles
+import '../css/TripList.css'
+
+export default function TripList() {
+    const [trips, setTrips] = useState([])
+
+    //Fetch Data When Component 1st Runs
+    useEffect(() => { 
+        fetch('http://localhost:3000/trips')
+        .then(response => response.json())
+        .then(json => setTrips(json))
+    }, [])
+
+    console.log(trips)
+    
+  return (
+    <div className='trip-list'>
+          <h2>Trip List</h2>
+          <ul>
+              {trips.map(trip => (
+                  <li key={trip.id}>
+                      <h3>{trip.title}</h3>
+                      <p>{trip.price}</p>
+                  </li>
+              ))}
+          </ul>
+    </div>
+  )
+}
